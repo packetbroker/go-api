b23e646c667bef3480b313ab529ff37c92c56bb8
Johan Stokking <johan@stokking.nl>
2020-11-04 20:53:45 +0100

    Add v1 routing and mapping

---
+++ routing/validate.go
@@ -0,0 +1,126 @@
+// Copyright © 2020 The Things Industries B.V.
+
+package routingpb
+
+import (
+	"errors"
+
+	packetbroker "go.packetbroker.org/api/v3"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+)
+
+// Validate returns whether the request is valid.
+func (r *ListDefaultPoliciesRequest) Validate() error {
+	if err := r.UpdatedSince.CheckValid(); err != nil && r.UpdatedSince != nil {
+		return status.Error(codes.InvalidArgument, "invalid updated_since")
+	}
+	return nil
+}
+
+// Validate returns whether the request is valid.
+func (r *GetDefaultPolicyRequest) Validate() error {
+	return packetbroker.ForwarderTenantID(r).Validate()
+}
+
+// Validate returns whether the request is valid.
+func (r *ListHomeNetworkPoliciesRequest) Validate() error {
+	if err := r.UpdatedSince.CheckValid(); err != nil && r.UpdatedSince != nil {
+		return status.Error(codes.InvalidArgument, "invalid updated_since")
+	}
+	return packetbroker.ForwarderTenantID(r).Validate()
+}
+
+// Validate returns whether the request is valid.
+func (r *GetHomeNetworkPolicyRequest) Validate() error {
+	if err := packetbroker.ForwarderTenantID(r).Validate(); err != nil {
+		return err
+	}
+	return packetbroker.HomeNetworkTenantID(r).Validate()
+}
+
+// Validate returns whether the request is valid.
+func (r *SetPolicyRequest) Validate() error {
+	if err := packetbroker.ForwarderTenantID(r.Policy).Validate(); err != nil {
+		return err
+	}
+	return packetbroker.HomeNetworkTenantID(r.Policy).Validate()
+}
+
+// Validate returns whether the request is valid.
+func (r *PublishUplinkMessageRequest) Validate() error {
+	if !packetbroker.ClusterIDRegex.MatchString(r.ForwarderClusterId) {
+		return errors.New("invalid Forwarder Cluster ID format")
+	}
+	return packetbroker.ForwarderTenantID(r).Validate()
+}
+
+// Validate returns whether the request is valid.
+func (r *PublishDownlinkMessageRequest) Validate() error {
+	if !packetbroker.ClusterIDRegex.MatchString(r.ForwarderClusterId) {
+		return errors.New("invalid Forwarder Cluster ID format")
+	}
+	if err := packetbroker.ForwarderTenantID(r).Validate(); err != nil {
+		return err
+	}
+	if !packetbroker.ClusterIDRegex.MatchString(r.HomeNetworkClusterId) {
+		return errors.New("invalid Home Network Cluster ID format")
+	}
+	return packetbroker.HomeNetworkTenantID(r).Validate()
+}
+
+// Validate returns whether the request is valid.
+func (r *SubscribeForwarderRequest) Validate() error {
+	if !packetbroker.ClusterIDRegex.MatchString(r.ForwarderClusterId) {
+		return errors.New("invalid Forwarder Cluster ID format")
+	}
+	if !packetbroker.SubscriptionGroupRegexp.MatchString(r.Group) {
+		return errors.New("invalid subscription group format")
+	}
+	return packetbroker.ForwarderTenantID(r).Validate()
+}
+
+// Validate returns whether the request is valid.
+func (r *SubscribeHomeNetworkRequest) Validate() error {
+	if !packetbroker.ClusterIDRegex.MatchString(r.HomeNetworkClusterId) {
+		return errors.New("invalid Home Network Cluster ID format")
+	}
+	if !packetbroker.SubscriptionGroupRegexp.MatchString(r.Group) {
+		return errors.New("invalid subscription group format")
+	}
+	return packetbroker.HomeNetworkTenantID(r).Validate()
+}
+
+// Validate returns whether the request is valid.
+func (r *RouteUplinkMessageRequest) Validate() error {
+	if r.Message == nil {
+		return errors.New("message is required")
+	}
+	if !packetbroker.ClusterIDRegex.MatchString(r.Message.ForwarderClusterId) {
+		return errors.New("invalid Home Network Cluster ID format")
+	}
+	if err := packetbroker.ForwarderTenantID(r.Message).Validate(); err != nil {
+		return err
+	}
+	if !packetbroker.ClusterIDRegex.MatchString(r.Message.HomeNetworkClusterId) {
+		return errors.New("invalid Home Network Cluster ID format")
+	}
+	return packetbroker.HomeNetworkTenantID(r.Message).Validate()
+}
+
+// Validate returns whether the request is valid.
+func (r *RouteDownlinkMessageRequest) Validate() error {
+	if r.Message == nil {
+		return errors.New("message is required")
+	}
+	if !packetbroker.ClusterIDRegex.MatchString(r.Message.ForwarderClusterId) {
+		return errors.New("invalid Home Network Cluster ID format")
+	}
+	if err := packetbroker.ForwarderTenantID(r.Message).Validate(); err != nil {
+		return err
+	}
+	if !packetbroker.ClusterIDRegex.MatchString(r.Message.HomeNetworkClusterId) {
+		return errors.New("invalid Home Network Cluster ID format")
+	}
+	return packetbroker.HomeNetworkTenantID(r.Message).Validate()
+}
--- v3/go.mod
+++ v3/go.mod
@@ -1,13 +1,13 @@
 module go.packetbroker.org/api/v3
 
-go 1.14
+go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.3
 	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
 	golang.org/x/sys v0.0.0-20201101102859-da207088b7d1 // indirect
 	golang.org/x/text v0.3.4 // indirect
-	google.golang.org/genproto v0.0.0-20201102152239-715cce707fb0 // indirect
+	google.golang.org/genproto v0.0.0-20201104152603-2e45c02ce95c // indirect
 	google.golang.org/grpc v1.33.1
 	google.golang.org/protobuf v1.25.0
 )
